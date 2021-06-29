1646702ba7f20c2a8563b42f2c06f20a7620d91e
DutchmanNL <rdrozda86@gmail.com>
2021-06-29 18:23:32 +0200

    chore: release v0.2.3
    Bugfix : Light component state not changed [#74](https://github.com/DrozmotiX/ioBroker.esphome/issues/74)
    Update compatibility to version 1.19.4 of ESPHome Dashboard

--- main.js
+++ main.js
@@ -1020,32 +1020,32 @@ class Esphome extends utils.Adapter {
 						this.deviceInfo[deviceIP][device[4]].states[device[5]] = writeValue;
 
 					} else if (device[5] === 'state') {
-                        			this.deviceInfo[deviceIP][device[4]].states.state = writeValue;
-                    			}
+						this.deviceInfo[deviceIP][device[4]].states.state = writeValue;
+					}
 
-					let data = {
+					const data = {
 						key: this.deviceInfo[deviceIP][device[4]].states.key,
-                        			state: this.deviceInfo[deviceIP][device[4]].states.state,
-                        			transitionLength: this.deviceInfo[deviceIP][device[4]].states.transitionLength
-                    			}
+						state: this.deviceInfo[deviceIP][device[4]].states.state,
+						transitionLength: this.deviceInfo[deviceIP][device[4]].states.transitionLength
+					};
 					if(this.deviceInfo[deviceIP][device[4]].config.supportsBrightness === true){
-					    data.brightness = this.deviceInfo[deviceIP][device[4]].states.brightness;
-                    			}
+						data.brightness = this.deviceInfo[deviceIP][device[4]].states.brightness;
+					}
 					if(this.deviceInfo[deviceIP][device[4]].config.supportsRgb === true){
-					    data.red = this.deviceInfo[deviceIP][device[4]].states.red;
-					    data.green = this.deviceInfo[deviceIP][device[4]].states.green;
-					    data.blue = this.deviceInfo[deviceIP][device[4]].states.blue;
-                    			}
+						data.red = this.deviceInfo[deviceIP][device[4]].states.red;
+						data.green = this.deviceInfo[deviceIP][device[4]].states.green;
+						data.blue = this.deviceInfo[deviceIP][device[4]].states.blue;
+					}
 					if(this.deviceInfo[deviceIP][device[4]].config.supportsWhiteValue === true){
-					    data.white = this.deviceInfo[deviceIP][device[4]].states.white;
-                    			}
+						data.white = this.deviceInfo[deviceIP][device[4]].states.white;
+					}
 					if(this.deviceInfo[deviceIP][device[4]].config.supportsColorTemperature === true){
-					    data.colorTemperature = this.deviceInfo[deviceIP][device[4]].states.colorTemperature;
-                    			}
+						data.colorTemperature = this.deviceInfo[deviceIP][device[4]].states.colorTemperature;
+					}
 					const effect = this.deviceInfo[deviceIP][device[4]].states.effect;
 					if(effect !== '' && effect !== null && effect !== undefined){
-					    data.effect = effect;
-                    			}
+						data.effect = effect;
+					}
 
 					this.log.debug(`Send Light values ${JSON.stringify(data)}`);
 					await client[deviceIP].connection.lightCommandService(data);
