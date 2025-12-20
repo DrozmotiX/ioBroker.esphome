d4ede451ea6a4196de3b5c06aefc5f05120a396e
Simon Fischer <simi.fischa@gmail.com>
2025-12-21 00:12:44 +0100

    remove useless files, use latest version

--- test/integrationTests/dashboard_tests.js
+++ test/integrationTests/dashboard_tests.js
@@ -17,7 +17,7 @@ const { expect } = require('chai');
 
 // Test configuration constants
 const DASHBOARD_PORT = 6052;
-const DASHBOARD_VERSION = '2024.12.0';
+const DASHBOARD_VERSION = 'Always last available';
 const DASHBOARD_INITIALIZATION_DELAY_MS = 30000; // 30 seconds for dashboard to initialize
 const REACHABILITY_CHECK_DELAY_MS = 3000; // 3 seconds between reachability checks
 const MAX_REACHABILITY_ATTEMPTS = 25; // Maximum attempts to check if dashboard is reachable
